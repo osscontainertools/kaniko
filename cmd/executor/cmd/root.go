02496f495cb4eb6d95245b3e7631d9b58da6efc5
ELADAV <eladav@rafael.co.il>
2024-11-19 15:19:30 +0200

    added oci-layout-path opt to relativePath check

--- cmd/executor/cmd/root.go
+++ cmd/executor/cmd/root.go
@@ -463,6 +463,7 @@ func resolveRelativePaths() error {
 		&opts.DigestFile,
 		&opts.ImageNameDigestFile,
 		&opts.ImageNameTagDigestFile,
+		&opts.OCILayoutPath,
 	}
 
 	for _, p := range optsPaths {
