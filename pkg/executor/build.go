7d6cb3bfb3850756bed919b14708bda36bd2cd7b
Martin Zihlmann <martizih@outlook.com>
2025-10-24 05:58:40 +0100

    move resolveCrossStageCommands forwards

--- pkg/executor/build.go
+++ pkg/executor/build.go
@@ -1006,8 +1006,6 @@ func fetchExtraStages(stages []config.KanikoStage, opts *config.KanikoOptions) e
 	t := timing.Start("Fetching Extra Stages")
 	defer timing.DefaultRun.Stop(t)
 
-	var names []string
-
 	for _, s := range stages {
 		for _, cmd := range s.Commands {
 			c, ok := cmd.(*instructions.CopyCommand)
@@ -1016,14 +1014,13 @@ func fetchExtraStages(stages []config.KanikoStage, opts *config.KanikoOptions) e
 			}
 
 			// FROMs at this point are guaranteed to be either an integer referring to a previous stage,
-			// the name of a previous stage, or a name of a remote image.
+			// or a name of a remote image.
 
-			// If it is an integer stage index, validate that it is actually a previous index
-			if fromIndex, err := strconv.Atoi(c.From); err == nil && s.Index > fromIndex && fromIndex >= 0 {
-				continue
-			}
-			// Check if the name is the alias of a previous stage
-			if fromPreviousStage(c, names) {
+			if fromIndex, err := strconv.Atoi(c.From); err == nil {
+				// If it is an integer stage index, validate that it is actually a previous index
+				if s.Index <= fromIndex || fromIndex < 0 {
+					return fmt.Errorf("%s refers to invalid stage: %d", c.String(), fromIndex)
+				}
 				continue
 			}
 
@@ -1039,10 +1036,6 @@ func fetchExtraStages(stages []config.KanikoStage, opts *config.KanikoOptions) e
 			if err := extractImageToDependencyDir(c.From, sourceImage); err != nil {
 				return err
 			}
-		}
-		// Store the name of the current stage in the list with names, if applicable.
-		if s.Name != "" {
-			names = append(names, s.Name)
 		}
 	}
 	return nil
