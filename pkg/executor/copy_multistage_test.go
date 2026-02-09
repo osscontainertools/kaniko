931950f46354c09676a73a3ea898e7dcfd0f4f18
Martin Zihlmann <martizih@outlook.com>
2026-02-09 20:16:24 +0000

    use a global instead

--- pkg/executor/copy_multistage_test.go
+++ pkg/executor/copy_multistage_test.go
@@ -18,7 +18,6 @@ package executor
 
 import (
 	"fmt"
-	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -64,7 +63,7 @@ COPY --from=first copied/bam.txt output/bam.txt`
 			SrcContext:     filepath.Join(testDir, "workspace"),
 			SnapshotMode:   constants.SnapshotModeFull,
 		}
-		_, err := DoBuild(opts, io.Discard)
+		_, err := DoBuild(opts)
 		testutil.CheckNoError(t, err)
 		// Check Image has one layer bam.txt
 		files, err := readDirectory(filepath.Join(testDir, "output"))
@@ -92,7 +91,7 @@ COPY --from=first copied/bam.txt output/`
 			SrcContext:     filepath.Join(testDir, "workspace"),
 			SnapshotMode:   constants.SnapshotModeFull,
 		}
-		_, err := DoBuild(opts, io.Discard)
+		_, err := DoBuild(opts)
 		testutil.CheckNoError(t, err)
 		files, err := readDirectory(filepath.Join(testDir, "output"))
 		if err != nil {
@@ -118,7 +117,7 @@ COPY --from=first copied another`
 			SrcContext:     filepath.Join(testDir, "workspace"),
 			SnapshotMode:   constants.SnapshotModeFull,
 		}
-		_, err := DoBuild(opts, io.Discard)
+		_, err := DoBuild(opts)
 		testutil.CheckNoError(t, err)
 		// Check Image has one layer bam.txt
 		files, err := readDirectory(filepath.Join(testDir, "another"))
@@ -153,7 +152,7 @@ COPY --from=first / output/`
 			SrcContext:     filepath.Join(testDir, "workspace"),
 			SnapshotMode:   constants.SnapshotModeFull,
 		}
-		_, err := DoBuild(opts, io.Discard)
+		_, err := DoBuild(opts)
 		testutil.CheckNoError(t, err)
 
 		filesUnderRoot, err := os.ReadDir(filepath.Join(testDir, "output/"))
