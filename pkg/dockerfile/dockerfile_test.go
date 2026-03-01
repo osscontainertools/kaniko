8b8aa5282e4e6b50dd6b2ea9db5d33fa0ae329b1
Martin Zihlmann <martizih@outlook.com>
2026-03-01 23:50:04 +0000

    fixup revive

--- pkg/dockerfile/dockerfile_test.go
+++ pkg/dockerfile/dockerfile_test.go
@@ -233,7 +233,7 @@ func Test_GetOnBuildInstructions(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			cmds, err := ParseCommands(test.cfg.OnBuild)
 			if err != nil {
-				errors.New("Failed to parse config for on-build instructions")
+				t.Error("Failed to parse config for on-build instructions")
 			}
 			if len(cmds) != len(test.expCommands) {
 				t.Fatalf("Expected %d commands, got %d", len(test.expCommands), len(cmds))
