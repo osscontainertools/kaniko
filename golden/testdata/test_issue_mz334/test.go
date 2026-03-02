21938b7198a084977b607578d4504b364ee3ac9e
Martin Zihlmann <martizih@outlook.com>
2026-03-02 20:30:36 +0000

    update tests

--- golden/testdata/test_issue_mz334/test.go
+++ golden/testdata/test_issue_mz334/test.go
@@ -15,6 +15,8 @@ var Tests = types.GoldenTests{
 			Args: []string{"--no-push", "--cache", "--cache-copy-layers"},
 			KeySequence: []string{
 				"b0db5c4dc0ee6072dc9f3e18194c1cf4745367521c6a6bb4a8ebb269be5e7c82",
+				"3576b2d5bfde80d864d0b057a67fea6796fcddc8c0d29875abbce46209d5a5eb",
+				"1997ccce84320d3b5363cb02a377748cc30451928a285d98fe8f80161b0b7ab1",
 			},
 			Plan: "cached",
 		},
