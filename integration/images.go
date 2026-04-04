70e2311101ee757f23ff5faf42a87eef8bd586de
Martin Zihlmann <martizih@outlook.com>
2026-04-04 12:32:35 +0100

    infer crosstage

--- integration/images.go
+++ integration/images.go
@@ -88,7 +88,7 @@ var envsMap = map[string][]string{
 	"Dockerfile_test_issue_mz473":                {"KANIKO_DIR=/kaniko2"},
 	"Dockerfile_test_issue_mz511":                {"FF_KANIKO_SQUASH_STAGES=0"},
 	"Dockerfile_test_issue_mz529":                {"FF_KANIKO_SQUASH_STAGES=0"},
-	"Dockerfile_test_issue_mz334_1":              {"FF_KANIKO_INFER_CROSS_STAGE_CACHE_KEY=1"},
+	"Dockerfile_test_issue_mz334":                {"FF_KANIKO_INFER_CROSS_STAGE_CACHE_KEY=1"},
 }
 
 var KanikoEnv = []string{
@@ -124,8 +124,7 @@ var additionalKanikoFlagsMap = map[string][]string{
 	"Dockerfile_test_maintainer":             {"--single-snapshot"},
 	"Dockerfile_test_target":                 {"--target=second"},
 	"Dockerfile_test_snapshotter_ignorelist": {"--use-new-run=true", "-v=trace"},
-	"Dockerfile_test_issue_mz334_1":          {"--cache-copy-layers=true"},
-	"Dockerfile_test_issue_mz334_2":          {"--cache-copy-layers=true"},
+	"Dockerfile_test_issue_mz334":            {"--cache-copy-layers=true"},
 	"Dockerfile_test_cache":                  {"--cache-copy-layers=true"},
 	"Dockerfile_test_cache_oci":              {"--cache-copy-layers=true"},
 	"Dockerfile_test_cache_install":          {"--cache-copy-layers=true"},
@@ -205,7 +204,7 @@ var outputChecks = map[string]func(string, []byte) error{
 }
 
 var cacheHitOutputChecks = map[string]func(string, []byte) error{
-	"Dockerfile_test_issue_mz334_1": func(_ string, out []byte) error {
+	"Dockerfile_test_issue_mz334": func(_ string, out []byte) error {
 		for _, cmd := range []string{
 			"COPY --from=first /blubb /blubb",
 			"COPY --from=third /bli /bli",
@@ -347,8 +346,7 @@ func NewDockerFileBuilder() *DockerFileBuilder {
 		"Dockerfile_test_issue_workdir": {},
 		"Dockerfile_test_issue_add":     {},
 		"Dockerfile_test_issue_empty":   {},
-		"Dockerfile_test_issue_mz334_1": {},
-		"Dockerfile_test_issue_mz334_2": {},
+		"Dockerfile_test_issue_mz334":   {},
 	}
 	d.TestOCICacheDockerfiles = map[string]struct{}{
 		"Dockerfile_test_cache_oci":         {},
