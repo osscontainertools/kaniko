7c012eaa136a1a23b376b990bec1ad0adcc91e63
Martin Zihlmann <martizih@outlook.com>
2026-04-19 19:39:58 +0200

    revert refactor

--- pkg/executor/build.go
+++ pkg/executor/build.go
@@ -70,8 +70,7 @@ type snapShotter interface {
 
 // stageBuilder contains all fields necessary to build one stage of a Dockerfile
 type stageBuilder struct {
-	index           int
-	final           bool
+	stage           config.KanikoStage
 	image           v1.Image
 	cf              *v1.ConfigFile
 	baseImageDigest string
@@ -137,8 +136,7 @@ func newStageBuilder(args *dockerfile.BuildArgs, opts *config.KanikoOptions, sta
 		return nil, err
 	}
 	s := &stageBuilder{
-		index:           stage.Index,
-		final:           stage.Final,
+		stage:           stage,
 		image:           sourceImage,
 		cf:              imageConfig,
 		baseImageDigest: digest.String(),
@@ -388,10 +386,10 @@ func (s *stageBuilder) build(compositeKey CompositeCache, opts *config.KanikoOpt
 	if crossStageDeps {
 		shouldUnpack = true
 	}
-	if s.final && opts.Materialize {
+	if s.stage.Final && opts.Materialize {
 		shouldUnpack = true
 	}
-	if s.index == 0 && opts.InitialFSUnpacked {
+	if s.stage.Index == 0 && opts.InitialFSUnpacked {
 		shouldUnpack = false
 	}
 
