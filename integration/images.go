a3b25c0d5b2056722fb9a058c873304ea341d097
Martin Zihlmann <martizih@outlook.com>
2026-04-25 01:04:27 +0100

    mz621: integration test for single-snapshot assertion violation

--- integration/images.go
+++ integration/images.go
@@ -166,6 +166,7 @@ var executorImages = map[string]string{
 // Arguments to build Dockerfiles with when building with kaniko
 var additionalKanikoFlagsMap = map[string][]string{
 	"Dockerfile_test_add":                    {"--single-snapshot"},
+	"Dockerfile_test_issue_mz621":            {"--single-snapshot"},
 	"Dockerfile_test_run_new":                {"--use-new-run=true"},
 	"Dockerfile_test_run_redo":               {"--snapshot-mode=redo"},
 	"Dockerfile_test_scratch":                {"--single-snapshot"},
