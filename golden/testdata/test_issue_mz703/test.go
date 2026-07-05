9c1463b870e0824d5d76692e4e91ebc66a20c38c
Martin Zihlmann <martizih@outlook.com>
2026-07-05 16:53:21 +0100

    unpin FF_KANIKO_NO_PROPAGATE_ANNOTATIONS in golden and rotate cache keys

--- golden/testdata/test_issue_mz703/test.go
+++ golden/testdata/test_issue_mz703/test.go
@@ -12,8 +12,8 @@ var Tests = types.GoldenTests{
 				"FF_KANIKO_CACHE_LOOKAHEAD": "1",
 			},
 			CachedKeys: []string{
-				"2b9fe1fa3aca624785f33c81b10e22fdd7957bbe1791e0b390432b7dcd8d3b3c",
-				"fbd83b67ae5fc062049c8f0f8caf849790fad221667b7b49db5bc5e53847dfbf",
+				"ef4ba1bfa1a8010630d9a007fad694d95d88419c791f0053b5525169f21e3247",
+				"2f4043dde38e8a86a388c786d43c46606463533a2eca79177a7246698f9b62a7",
 			},
 			Plan: "legacy_stop_after_miss",
 		},
@@ -24,8 +24,8 @@ var Tests = types.GoldenTests{
 				"FF_KANIKO_CACHE_PROBE_AFTER_MISS": "1",
 			},
 			CachedKeys: []string{
-				"2b9fe1fa3aca624785f33c81b10e22fdd7957bbe1791e0b390432b7dcd8d3b3c",
-				"fbd83b67ae5fc062049c8f0f8caf849790fad221667b7b49db5bc5e53847dfbf",
+				"ef4ba1bfa1a8010630d9a007fad694d95d88419c791f0053b5525169f21e3247",
+				"2f4043dde38e8a86a388c786d43c46606463533a2eca79177a7246698f9b62a7",
 			},
 			Plan: "probe_after_miss",
 		},
